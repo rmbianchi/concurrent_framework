e351e71e076f66f1060269eb6511799bf4f81fa7
Benedikt Hegner <benedikt.hegner@cern.ch>
2012-04-12 14:26:32 +0200

    adding a separate class AlgoPool to handle the available algo instances

---
+++ AlgoPool.cpp
@@ -0,0 +1,9 @@
+//
+//  AlgoPool.cpp
+//  
+//
+//  Created by Benedikt Hegner on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ AlgoPool.h
@@ -0,0 +1,14 @@
+//
+//  AlgoPool.h
+//  
+//
+//  Created by Benedikt Hegner on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _AlgoPool_h
+#define _AlgoPool_h
+
+
+
+#endif
