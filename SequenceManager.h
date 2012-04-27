69e41880831ecfdb26b31d4d672f411894af387c
R.M.Bianchi <rmbianchi@libero.it>
2012-04-27 15:13:42 +0200

    - Fixed a strange include chain with Helpers. He needed to have the ExampleChains.h in its include chain to get access to the sys/time.h header. Now I explicitely added the sys/time.h header to the Helpers.h header and fixed it. - Added headers to the CMakeList.txt file in order to have the .h files included in the XCode project when generated with CMake.

---
+++ SequenceManager.h
@@ -0,0 +1,14 @@
+//
+//  SequenceManager.h
+//  CF4Hep
+//
+//  Created by Riccardo Maria Bianchi on 4/27/12.
+//  Copyright (c) 2012 R.M.Bianchi. All rights reserved.
+//
+
+#ifndef CF4Hep_SequenceManager_h
+#define CF4Hep_SequenceManager_h
+
+
+
+#endif
